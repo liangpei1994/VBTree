fb9587c9c6dca05687d46ffb31f14f4e9591afb6
ZHANG Chen <lacare1192@gmail.com>
2018-09-05 20:20:42 +0900

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+
+#' Title testTitle
+#'
+#' @param x bbbsdfs
+#' @param y sdfsf
+#' @param z sdfsdf
+#'
+#' @return cxdsdfs
+#' @export sadfasd
+#'
+#' @examples sadfs dd
+hello <- function(x, y, z) {
+  b <- x + y*z
+  print("Hello, world!")
+}
